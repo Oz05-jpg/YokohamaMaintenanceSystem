571c3eecf8e80ad6b9008e8c00b0ad7ccb61ae5f
warut mumchanda <warutmcd05@gmail.com>
2026-05-31 15:35:03 +0900

    feat: add Dashboard summary page with machine and request counts

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YokohamaMaintenanceSystem.Data;
+using YokohamaMaintenanceSystem.Models;
+
+namespace YokohamaMaintenanceSystem.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly AppDbContext _context;
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var machinCount = _context.Machines.Count();
+            var openRequests = _context.MaintenanceRequests
+                               .Count(r => r.Status == "Open");
+            var completedRequests = _context.MaintenanceRequests.Count(r => r.Status == "Completed");
+
+            var vm = new DashboardViewModel
+            {
+                MachinCount = machinCount,
+                OpenRequests = openRequests,
+                CompletedRequests = completedRequests
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace YokohamaMaintenanceSystem.Models
+{
+    public class DashboardViewModel
+    {
+        public int MachinCount { get; internal set; }
+        public int CompletedRequests { get; internal set; }
+        public int OpenRequests { get; internal set; }
+    }
+}
