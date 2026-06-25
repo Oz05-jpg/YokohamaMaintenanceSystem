e52833269ae760601b6854e5d57a961c1c062ea5
warut mumchanda <warutmcd05@gmail.com>
2026-06-25 20:30:42 +0900

    feat: TICKET #018 Export Maintenance Report to Excel (ClosedXML)

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using YokohamaMaintenanceSystem.Data;
 using YokohamaMaintenanceSystem.Enums;
+using YokohamaMaintenanceSystem.Interfaces;
 using YokohamaMaintenanceSystem.Models;
 
 namespace YokohamaMaintenanceSystem.Controllers
@@ -12,9 +14,12 @@ namespace YokohamaMaintenanceSystem.Controllers
     {
 
         private readonly AppDbContext _context;
-        public ReportController(AppDbContext context)
+        private readonly IMaintenanceRequestRepository _repo;
+        public ReportController(AppDbContext context,
+            IMaintenanceRequestRepository repo)
         {
             _context = context;
+            _repo = repo;
         }
 
         public async Task<IActionResult> Index()
@@ -29,6 +34,41 @@ namespace YokohamaMaintenanceSystem.Controllers
             };
             return View(vm);
         }
+
+        public async Task<IActionResult> ExportExcel()
+        {
+            var requests = await _context.MaintenanceRequests
+              .Include(r => r.Machine)
+              .ToListAsync();
+
+            using var workbook = new XLWorkbook();
+            var ws = workbook.Worksheets.Add("Report");
+
+            //Header row (row 1)
+            ws.Cell(1, 1).Value = "Id";
+            ws.Cell(1, 2).Value = "Title";
+            ws.Cell(1, 3).Value = "Status";
+            ws.Cell(1, 4).Value = "MachineName";
+            ws.Cell(1, 5).Value = "CreatedAt";
+
+            // Data rows (เริ่มที่ row 2)
+            int row = 2;
+            foreach (var r in requests)
+            {
+                ws.Cell(row, 1).Value = r.Id;
+                ws.Cell(row, 2).Value = r.Title;
+                ws.Cell(row, 3).Value = r.Status.ToString();   // Status เป็น enum
+                ws.Cell(row, 4).Value = r.Machine?.Name ?? "N/A"; // MachineName (nullable)
+                ws.Cell(row, 5).Value = r.CreatedAt.ToString("yyyy-MM-dd");
+                row++;
+            }
+            using var stream = new MemoryStream();
+            workbook.SaveAs(stream);
+            var bytes = stream.ToArray();
+
+            return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "report.xlsx");
+        }
+
 
     }
 }
